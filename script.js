53bca0a46aabc981f32e3a2e3599cf514eb9b359
Cheng <yucheng9824@gmail.com>
2026-09-07 11:10:02 +0800

    Add grid style div

--- script.js
+++ script.js
@@ -2,5 +2,8 @@ const container = document.querySelector("#container");
 
 for (let i = 0; i < 256; i++) {
     const item = document.createElement("div");
+
+    item.classList.add('item');
+
     container.appendChild(item);
 }
