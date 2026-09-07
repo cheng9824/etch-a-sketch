78202bdffd280e89d42bde5f83cc07efefd67964
Cheng <yucheng9824@gmail.com>
2026-09-07 12:27:53 +0800

    Adjust code order

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
 const container = document.querySelector("#container");
 
-for (let i = 0; i < 256; i++) {
-    const item = document.createElement("div");
+for (let i = 0; i < 16 * 16; i++) {
+    const div = document.createElement("div");
 
-    item.classList.add('item');
+    div.classList.add('item');
 
-    container.appendChild(item);
+    container.appendChild(div);
+}
+
+const button = document.querySelector("button");
+let size;
+
+button.addEventListener("click", () => {
+    size = Number(prompt("Change your size"));
+
+    if (size < 1 || size > 100) {
+        size = Number(prompt("change your size"))
+    } else {
+        container.removeChild(div);
+        createGrid();
+    }
+})
+
+function createGrid() {
+    for (let i = 0; i < size * size; i++) {
+        const div = document.createElement("div");
+
+        div.classList.add('item');
+
+        container.appendChild(div);
+    }
 }
 
 const items = document.querySelectorAll(".item");
