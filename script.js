01db2ece41962eb03459c12cb02abfaaf7664e61
Cheng <yucheng9824@gmail.com>
2026-09-07 15:15:00 +0800

    Add remove container child function

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const container = document.querySelector("#container");
 
 for (let i = 0; i < 16 * 16; i++) {
     const div = document.createElement("div");
-
     div.classList.add('item');
 
     container.appendChild(div);
@@ -14,11 +13,16 @@ let size;
 button.addEventListener("click", () => {
     size = Number(prompt("Change your size"));
 
-    if (size < 1 || size > 100) {
-        size = Number(prompt("change your size"))
-    } else {
-        container.removeChild(div);
-        createGrid();
+    while (true) {
+        if (size < 1 || size > 100) {
+            size = Number(prompt("change your size"))
+        } else {
+            while (container.firstChild) {
+                container.removeChild(container.firstChild);
+            }
+            createGrid();
+            break;
+        }
     }
 })
 
@@ -29,6 +33,14 @@ function createGrid() {
         div.classList.add('item');
 
         container.appendChild(div);
+
+        const items = document.querySelectorAll(".item");
+
+        items.forEach((item) => {
+            item.addEventListener("mouseover", () => {
+                item.style.backgroundColor = "grey";
+            });
+        })
     }
 }
 
